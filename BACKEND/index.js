f3f35a140c15af372449a29802ba500e975c63c8
Lakshan Wickramage <it22606310@my.sliit.lk>
2025-04-30 21:02:19 +0530

    cretae the add product page full co0de

--- BACKEND/index.js
+++ BACKEND/index.js
@@ -1,7 +1,10 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
-
+import userRoutes from "./routes/user.route.js";
+import authRoutes from "./routes/auth.route.js";
+import productRoutes from "./routes/product.route.js"; 
+import supplyRoutes from "./routes/supply.route.js"; 
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
 
@@ -26,7 +29,10 @@ app.listen(3000, () => {
   console.log("Server is running on port 3000!!");
 });
 
-
+app.use('/api/user', userRoutes);
+app.use('/api/auth', authRoutes);
+app.use('/api/product', productRoutes);
+app.use('/api/supply', supplyRoutes);
 
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
